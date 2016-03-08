c4351fb9daa2b05db7fcd3075fd55ca7277d7d47
imshyam <sschoudhary0001@gmail.com>
2016-03-09 04:56:27 +0530

    Initialized settings

--- app/src/main/java/com/shapps/ytube/MainActivity.java
+++ app/src/main/java/com/shapps/ytube/MainActivity.java
@@ -219,6 +219,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/com/shapps/ytube/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.shapps.ytube;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
